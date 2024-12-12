6d887f94be8bb76db6496884fed46a3700d1caf0
pedrogomes107 <pedromcgomes95@gmail.com>
2024-12-12 13:14:50 +0000

    Socket Url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://localhost:5000", {
+      const socket = io("https://mern-chat-app-17im.onrender.com", {
         query: {
           userId: authUser._id,
         },
